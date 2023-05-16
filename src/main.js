f8e9ab631da983192bf08648ffce72b895ef5268
Anibal Ventura <anibalventura123@gmail.com>
2023-05-16 11:59:27 -0500

    Update project

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const Stellar = require("./protocol/stellar/Stellar");
 const StellarDowngrade = require("./protocol/stellar_downgrade/StellarDowngrade");
 const {excecuteAndMonitoringProtocol} = require("./helpers/GeneralHelpers");
 
-//  Test config
+//  Config testing algorithms
 const secondsTest = [20];
 const nodesTest = [500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000, 16000,17000, 18000, 19000, 20000];
 
