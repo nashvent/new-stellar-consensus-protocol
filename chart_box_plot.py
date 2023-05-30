77c34422a540e9c174c3996c82267a690312ce7e
Anibal Ventura <anibalventura123@gmail.com>
2023-05-30 17:11:35 -0500

    Add boxplot chart

--- chart_box_plot.py
+++ chart_box_plot.py
@@ -1,30 +1,51 @@
 import matplotlib.pyplot as plt
 import numpy as np
- 
+import json
 np.random.seed(10)
-data_1 = np.random.normal(100, 10, 16)
-data_2 = np.random.normal(90, 20, 16)
-data_3 = np.random.normal(80, 30, 16)
-data_4 = np.random.normal(70, 40, 16)
 
-data_5 = np.random.normal(100, 10, 16)
-data_6 = np.random.normal(90, 20, 16)
-data_7 = np.random.normal(80, 30, 16)
-data_8 = np.random.normal(70, 40, 16)
+def getJsonFile(filename):
+    with open(filename) as f:
+        data = json.load(f)
+    return data
 
- 
+def concateningData(filenames, labels):
+    result = {}
+    for label in labels:
+        result[label] = []
+
+    for filename in filenames:
+        data = getJsonFile(filename)
+        for label in labels:
+            result[label].append(data['20'][str(label)]['consensusTime'])
+    return result
+
+
+xvalues = np.array(list(map(lambda x: int(x), getJsonFile('results/stellar_20.json')['20'].keys())))
+result_stellar = concateningData(['results/stellar_20.json'] + [f'results/stellar_20_{i}.json' for i in range(1, 6)], xvalues)
+result_stellar_downgrade = concateningData(['results/stellar_downgrade_20.json'] + [f'results/stellar_downgrade_20_{i}.json' for i in range(1, 6)], xvalues)
+result_dpos = concateningData(['results/dpos_20.json'] + [f'results/dpos_20_{i}.json' for i in range(1, 6)], xvalues)
+
 fig = plt.figure(figsize =(10, 7))
+plt.xticks(rotation=90)
 ax = fig.add_subplot(111)
- 
-bp = ax.boxplot([data_1, data_2, data_3, data_4], patch_artist = True,
-                notch ='True')
 
-bp = ax.boxplot([data_5, data_6, data_7, data_8], patch_artist = True,
-                notch ='True')
-colors = ['#0000FF', '#00FF00',
-          '#FFFF00', '#FF00FF']
+
+ax.boxplot(result_stellar_downgrade.values(), patch_artist = True,
+                notch ='True', labels=xvalues)
+
+ax.boxplot(result_dpos.values(), patch_artist = True,
+                notch ='True', labels=xvalues)
  
-for patch, color in zip(bp['boxes'], colors):
+bp = ax.boxplot(result_stellar.values(), patch_artist = True,
+                notch ='True', labels=xvalues)
+
+
+colors = [['#0000FF'] * len(xvalues) + ['#00FF00'] * len(xvalues) +
+          ['#FF0000'] * len(xvalues)]
+
+print(colors)
+
+for patch, color in zip(bp['boxes'], colors[0]):
     patch.set_facecolor(color)
 
 plt.title("Consensus algorithms")
